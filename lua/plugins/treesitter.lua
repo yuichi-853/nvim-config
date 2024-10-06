90f25da25b967d9526c7b1982f44c901bb7c5134
Tsunoda Yuichi <yuichi.bwv853@gmail.com>
2024-10-06 14:35:24 +0900

    Add some packages

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
 		local configs = require("nvim-treesitter.configs")
 
 		configs.setup({
-			ensure_installed = { "bash", "c", "cmake", "cpp", "css", "go", "gomod", "gosum", "gotmpl", "html", "javascript", "json", "lua", "sql", "vim", "vimdoc" },
+			ensure_installed = { "bash", "c", "cmake", "cpp", "css", "go", "gomod", "gosum", "gotmpl", "html", "javascript", "json", "lua", "php", "sql", "vim", "vimdoc" },
 			sync_install = false,
 			ignore_install = { "org" },
 			highlight = { enable = true },
