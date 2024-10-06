5f9e81e68dc9ac42af9cd26b1c210839a032ea5a
Tsunoda Yuichi <yuichi.bwv853@gmail.com>
2024-10-06 09:59:19 +0900

    Add wonderful vim-fugitive

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,9 +1,5 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
-
--- neo-tree
-vim.keymap.set("n", "<C-n>", ":Neotree<CR>", { desc = 'Toggle Neo-tree file explorer' })
 
 -- Set tab width to 4 spaces
 vim.opt.tabstop = 4
