89d18b1e7db254a692584f42baa6fa69eb64ac8a
Tsunoda Yuichi <yuichi.bwv853@gmail.com>
2024-10-09 12:08:00 +0900

    Enable completion

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -3,10 +3,6 @@ return {
 	dependencies = {
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
-		{ "VonHeikemen/lsp-zero.nvim", branch = "v4.x" },
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/nvim-cmp",
-		"L3MON4D3/LuaSnip",
 	},
 	config = function()
 		require("mason").setup()
@@ -20,46 +16,6 @@ return {
 				"ts_ls",
 				"eslint",
 			},
-		})
-
-		local lsp_zero = require("lsp-zero")
-		local lsp_attach = function(client, bufnr)
-			-- this is where you enable features that only work
-			-- if there is a language server active in the file
-		end
-		lsp_zero.extend_lspconfig({
-			sign_text = true,
-			lsp_attach = lsp_attach,
-			capabilities = require("cmp_nvim_lsp").default_capabilities(),
-		})
-
-		local cmp = require("cmp")
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					require("luasnip").lsp_expand(args.body)
-				end,
-			},
-			window = {
-				completion = cmp.config.window.bordered(),
-				documentation = cmp.config.window.bordered(),
-			},
-			mapping = cmp.mapping.preset.insert({
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(),
-				["<C-e>"] = cmp.mapping.abort(),
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
-			}),
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" }, -- For luasnip users.
-				-- { name = "vsnip" }, -- For vsnip users.
-				-- { name = 'ultisnips' }, -- For ultisnips users.
-				-- { name = 'snippy' }, -- For snippy users.
-			}, {
-				{ name = "buffer" },
-			}),
 		})
 
 		local lspconfig = require("lspconfig")
